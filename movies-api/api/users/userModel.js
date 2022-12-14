3b75d9c7e409a294780409ec7aaa1002096963ac
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2022-12-14 22:36:28 +0000

    Top[ic 11 - Salt and Hash.

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -1,28 +1,50 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt-nodejs';
 
 const Schema = mongoose.Schema;
 
 const MovieSchema = new Schema({
-    id: Number,
-    title: String
-  });
-  
-  const UserSchema = new Schema({
-    username: { type: String, unique: true, required: true},
-    password: {type: String, required: true },
-    favourites: [MovieSchema]
-  });
+  id: Number,
+  title: String
+});
 
-  UserSchema.statics.findByUserName = function (username) {
-    return this.findOne({ username: username });
-  };
-  
-  UserSchema.methods.comparePassword = function (candidatePassword) {
-    const isMatch = this.password === candidatePassword;
-    if (!isMatch) {
-      throw new Error('Password mismatch');
+const UserSchema = new Schema({
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  favourites: [MovieSchema]
+});
+
+UserSchema.statics.findByUserName = function (username) {
+  return this.findOne({ username: username });
+};
+
+UserSchema.methods.comparePassword = function (passw, callback) {
+  bcrypt.compare(passw, this.password, (err, isMatch) => {
+    if (err) {
+      return callback(err);
     }
-    return this;
-  };
+    callback(null, isMatch);
+  });
+};
+
+UserSchema.pre('save', function (next) {
+  const user = this;
+  if (this.isModified('password') || this.isNew) {
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) {
+        return next(err);
+      }
+      bcrypt.hash(user.password, salt, null, (err, hash) => {
+        if (err) {
+          return next(err);
+        }
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    return next();
+  }
+});
 
 export default mongoose.model('User', UserSchema);
