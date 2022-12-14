57566e5bacc0140beecde23e735d2b75c88135e6
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2022-12-14 22:52:57 +0000

    Topic 11 - Passport.

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import session from 'express-session';
-import authenticate from './authenticate';
+import passport from './authenticate';
 import moviesRouter from './api/movies';
 import genresRouter from './api/genres';
 import usersRouter from './api/users';
@@ -24,13 +24,8 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use(session({
-  secret: 'ilikecake',
-  resave: true,
-  saveUninitialized: true
-}));
-app.use('/api/movies', authenticate, moviesRouter);
-// app.use('/api/movies', moviesRouter);
+app.use(passport.initialize());
+app.use('/api/movies', passport.authenticate('jwt', {session: false}), moviesRouter);
 app.use('/api/genres', genresRouter);
 app.use('/api/users', usersRouter);
 app.use(errHandler);
