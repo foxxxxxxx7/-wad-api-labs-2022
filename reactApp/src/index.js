e604ae5e04a66fa6cf9b93bd8b0dffa38a4d09d9
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2022-12-15 17:19:18 +0000

    Topic 12 - Auth.

--- reactApp/src/index.js
+++ reactApp/src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes, Navigate, Link } from "react-router-dom";
 import { PublicPage, Movies, Profile, HomePage } from "./pages";
 
+import LoginPage from "./loginPage";
+import AuthProvider from "./authContext";
+import AuthHeader from "./authHeader";
+import ProtectedRoutes from "./protectedRoutes";
+
 const App = () => {
   return (
     <BrowserRouter>
+      <AuthProvider>
+        <AuthHeader />
         <ul>
           <li>
             <Link to="/">Home</Link>
@@ -21,12 +28,18 @@ const App = () => {
           </li>
         </ul>
         <Routes>
-          <Route path="/public" element={ <PublicPage /> } />
-          <Route path="/" element={ <HomePage /> } />
-          <Route path="/movies" element={ <Movies /> } />
-          <Route path="/profile" element={ <Profile /> } />
-          <Route path="*" element={ <Navigate to="/" /> } />
+          <Route path="/public" element={<PublicPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
